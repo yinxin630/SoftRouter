a07cafa2cd12ea093b56b268c992b8579fe3da99
yinxin630 <yinxin630@gmail.com>
2015-03-21 22:19:26 +0800

    添加SharpPcap DLL
    拷贝SharpPcap动态链接库文件，并在程序中添加引用以及using指令

--- SoftRouter/SoftRouter/Program.cs
+++ SoftRouter/SoftRouter/Program.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharpPcap;
+using PacketDotNet;
 
 namespace SoftRouter
 {
-	class Program
+	class SoftRouter
 	{
 		static void Main(string[] args)
 		{
